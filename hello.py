86ea0796220facac9a7aea5452caa6ced1d59f3f
Dima Sapsai <dima.sapsay@gmail.com>
2020-12-13 20:56:56 +0200

    3a

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,9 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>'
-
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return 'Hello, {}'.format(name)
+    return render_template('user.html', name=name)
