ed677670a2134102818386b561104e1fbb0b4dae
sapsai <sapsai@ksolutions.com.ua>
2020-12-14 17:22:54 +0200

    4a

--- hello.py
+++ hello.py
@@ -1,22 +1,39 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Hard_key'
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
