6801326dc4f0c3eb0e732236072388668a0d6517
sapsai <sapsai@ksolutions.com.ua>
2021-01-06 15:46:24 +0200

    6a

--- hello.py
+++ hello.py
@@ -1,29 +1,56 @@
 import os
+from configparser import ConfigParser
 
 from flask import Flask, render_template, session, \
     redirect, url_for, flash
-
 from flask_migrate import Migrate
-
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_mail import Mail, Message
 from flask_bootstrap import Bootstrap
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+config = ConfigParser()
+config.read('config.ini')
+
+secret_key = config.get('db', 'secret_key')
+db_name = config.get('db', 'db_name')
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'Hard_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SECRET_KEY'] = secret_key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, db_name)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['MAIL_SERVER'] = config.get('mail', 'mail_server')
+app.config['MAIL_PORT'] = config.get('mail', 'mail_port')
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = config.get('mail', 'mail_username')
+app.config['MAIL_PASSWORD'] = config.get('mail', 'mail_password')
+
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = config.get('mail', 'flasky_admin')
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(
+        app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+        sender=app.config['FLASKY_MAIL_SENDER'],
+        recipients=[to]
+    )
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+mail = Mail(app)
 bootstrap = Bootstrap(app)
 
 
@@ -56,12 +83,21 @@ class NameForm(FlaskForm):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
+        else:
+            session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/user/<name>')
